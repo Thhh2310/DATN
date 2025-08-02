df13408b6016f43862fd16890d1134a5542f4505
Thhh2310 <142483747+Thhh2310@users.noreply.github.com>
2025-08-03 00:54:42 +0700

    Delete Pet_Care directory

--- Pet_Care/Pet_Care/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Pet_Care.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
